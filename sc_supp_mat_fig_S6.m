839d552ca8f67f0bdd0a766ac9bbbf1bafa55dae
caravn15 <cara.victoria.neal@gmail.com>
2026-08-13 17:09:02 +0100

    Updated figure ordering

--- sc_supp_mat_fig_S6.m
+++ sc_supp_mat_fig_S6.m
@@ -1,7 +1,4 @@
 %% RUNS SIMULATIONS FOR AND PLOTS FIG. S6 OF THE SUPPLEMENTARY MATERIAL
-% USES THE SAME DATA AS FIG. S3 OF THE SUPPLEMENTARY MATERIAL -
-% YOU ONLY NEED TO RUN ONE SET OF SIMULATIONS (THIS SCRIPT OR
-% sc_supp_mat_fig_S3.m)
 
 clear all;
 close all;
@@ -10,81 +7,119 @@ clc; addpath(genpath('./'));
 %% parameters
 
 % fluid parameters
-Da_v          = [0.5e-2,1e-3,0.5e-3,1e-4]; % Darcy number (line plots are performed for second value)
-phm           = 0.65;                      % maximum packing fraction (must be <= 1)
-phin_v        = [0.46,0.5];                % inlet particle volume fraction (must be < phim)
-Qt            = 1;                         % total flow rate (scaled to 1 in non-dimensionalisation)
-delP          = [NaN 0];                   % pressure drop (delP(1) = NaN as Qt prescribed)
+Da            = 1e-5;                     % Darcy number
+phm_v         = [0.65,1];                 % maximum packing fraction vector (values must be <= 1)
+phin          = 0.36;                     % inlet particle volume fraction (must be < phim)
+Qt            = 1;                        % total flow rate (scaled to 1 in non-dimensionalisation)
+delP          = [NaN 0];                  % pressure drop (delP(1) = NaN as Qt prescribed)
+
+% constriction options
+consInds      = LoadConsIndsFig6();       % indices of vessels to reduce R
+consMultipler = 1e-2;                     % multiplier for R in clogged vessels (must be <= 1)
 
 % solution parameters
-tol           = 1e-6;                      % tolerance for iterations (epsilon in manuscript)
-maxit         = 50;                        % maximum number of iterations
+tol           = 1e-6;                     % tolerance for iterations (epsilon in manuscript)
+maxit         = 50;                       % maximum number of iterations
 
 % network information
-Netfld        = 'motif-fig-S6/';           % folder where data is stored
-NetType       = 'motif';                   % type of network: 'retinal-vasc' or 'motif'
+Netfld        = 'Brown-et-al-retina/';    % folder where data is stored
+NetType       = 'retinal-vasc';           % type of network: 'retinal-vasc' or 'motif'
+RetSec        = 'arteries';               % section of eye network to load in 'retinal-vasc'
 
-% save options  
-fld           = 'supp-mat-results/';       % folder to save data/figures
-fn            = 'fig_S6';                  % filename for data
-SqueezeData   = 1;                         % =0 to save all data (solutions at all iterations + diagnostic information)
-                                           % =1 to only save final solutions (results in smaller file sizes)
+% save options
+fld           = 'supp-mat-results/';      % folder to save data/figures
+fn            = 'fig_S6';                 % filename for data
+SqueezeData   = 1;                        % =0 to save all data (solutions at all iterations + diagnostic information)
+                                          % =1 to only save final solutions (results in smaller file sizes)
 
 % other options
-NumDivLines   = 500;                       % number of grid points for R_2,4 in simulations/plots (set =500 for paper results)
-NumDivSweeps  = 100;                       % number of grid points in each direction for phin/R_2,4 sweeps in simulations/plots (set =100 for paper results)
-phinLim       = [0.4,0.55];                % range of phin in phin/R_2,4 sweeps
-OutputCW      = 0;                         % output full solver details in the command window (set =0 if not required)
-DataLoad      = 0;                         % set =0 to run solve + plot, or =1 to load previous simulation data (finds path automatically)
+OutputCW      = 1;                        % output full solver details in the command window (set =0 if not wanted)
+DataLoad      = {};         
+DataLoad      = {'fig_S6_Da_1e-05_phin_0.36_phm_0.65','fig_S6_Da_1e-05_phin_0.36_phm_1'};
 
-%% check motif network data exists (generate if not)
+% For DataLoad, set as empty {} to run solve + plot, or provide path to sim. data in fld to skip solve
+% Data format: cell e.g. {'fig_S6_Da_1e-05_phin_0.36_phm_0.65','fig_S6_Da_1e-05_phin_0.36_phm_1'}
+% Do NOT append _clogging/_initcond/_newtonian
 
-try
-    load(['data/net/',Netfld,'data_net_motif.mat'],'net');
-catch
-    GenerateMotifFig3(); % same motif as fig. 3
-end
+%% solve for each phm (if required)
 
-%% solve (if required)
+if isempty(DataLoad)
 
-if DataLoad == 0
-
-    % solve required - same solve as fig. 3
-    [sol,nClogSweep]= SolveFig3(NumDivLines,NumDivSweeps,Netfld,NetType,Da_v,phm,phin_v,Qt,delP,phinLim,tol,maxit,fld,fn,SqueezeData,OutputCW);
-
+    % solve required
+    for ii=1:length(phm_v)
+        [net,SolClog_v{ii},SolNewt_v{ii},InitCond_v{ii},fnFull_v{ii}] = SolveFullNetworkClogging(Netfld,NetType,RetSec,...
+            Da,phm_v(ii),phin,Qt,delP,consInds,consMultipler,tol,maxit,fld,fn,SqueezeData,OutputCW);
+    end
 else
 
-    % load network data and get filename
-    net    = LoadNetworkData(Netfld,NetType,[]);
+    % simulation data provided - check format
+    if length(DataLoad) ~= length(phm_v) || ~iscell(DataLoad)
+        error(['Must provide path to ',num2str(length(phm_v)),' data files in cell format.']);
+    else
 
-    % load data
-    try
-        [sol,nClogSweep] = LoadFig3(NumDivLines,NumDivSweeps,Da_v,phm,phin_v,phinLim,fld,fn);
-    catch
-        error('Could not load data.');
+        % load data
+        for ii=1:length(phm_v)
+
+            % load network data and get filename
+            net          = LoadNetworkData(Netfld,NetType,RetSec);
+            fnFull_v{ii} = DataLoad{ii};
+
+            % load simulation data
+            try
+                load([fld,DataLoad{ii},'_newtonian.mat'],'SolNewt');
+                load([fld,DataLoad{ii},'_initcond.mat'],'InitCond');
+                load([fld,DataLoad{ii},'_clogging.mat'],'SolClog'); 
+
+                SolNewt_v{ii}  = SolNewt;
+                InitCond_v{ii} = InitCond;
+                SolClog_v{ii}  = SolClog;
+            catch
+                error(['Could not load data from: ',fld,DataLoad{ii}]);
+            end
+        end
     end
 end
 
-%% plot (and save) figures
+%% plot (and save) figures for each phm
 
-% create grid points for R_4,8
-R24_v = linspace(1,0.001,NumDivLines);
+for ii=1:length(phm_v)
 
-% plot figure
-figSup = figure('Theme','light','Position',[365,194,1267,671]);
-tiledlayout(2,3,'Padding','tight')
-nexttile; PlotFig3LinesF(1,sol,R24_v,1,1,phm)
-nexttile; PlotFig3LinesF(2,sol,R24_v,1,1,phm)
-nexttile; PlotFig3LinesF(3,sol,R24_v,1,1,phm)
-nexttile; PlotFig3LinesQ(1,sol,R24_v,1,1,phm)
-nexttile; PlotFig3LinesQ(2,sol,R24_v,1,1,phm)
-nexttile; PlotFig3LinesQ(3,sol,R24_v,1,1,phm)
+    % plot suspension F figure
+    figure('Theme','light','Position',[1219, 198, 523, 491]);
+    PlotSolutionDotsEdges(net,'arteries',SolClog_v{ii}.F,[1e-8,1e-0],[1e-8,1e-6,1e-4,1e-2,1e-0],1,'Solid flux $F$',consInds,1);
 
-% save panel (b)
-try
-    print(gcf, ['images/',fld,'image_',fn,'.png'],'-dpng','-r300');
-catch
-    mkdir(['images/',fld]);
-    print(gcf, ['images/',fld,'image_',fn,'.png'],'-dpng','-r300');
+    % save suspension F figure
+    try
+        print(gcf, ['images/',fld,'image_',fnFull_v{ii},'_clogged.png'],'-dpng','-r300');
+    catch
+        mkdir(['images/',fld]);
+        print(gcf, ['images/',fld,'image_',fnFull_v{ii},'_clogged.png'],'-dpng','-r300');
+    end
+
+    % plot newtonian F figure
+    figure('Theme','light','Position',[1219, 198, 523, 491]);
+    PlotSolutionDotsEdges(net,'arteries',SolNewt_v{ii}.F,[1e-8,1e-0],[1e-8,1e-6,1e-4,1e-2,1e-0],1,'Solid flux $F$',consInds,1);
+
+    % save newtonian F figure
+    try
+        print(gcf, ['images/',fld,'image_',fnFull_v{ii},'_newtonian.png'],'-dpng','-r300');
+    catch
+        mkdir(['images/',fld]);
+        print(gcf, ['images/',fld,'image_',fnFull_v{ii},'_newtonian.png'],'-dpng','-r300');
+    end
+
+    % plot phi figure
+    figure('Theme','light','Position',[1219, 198, 523, 491]);
+    PlotSolutionDotsEdges(net,'arteries',SolClog_v{ii}.ph,[0,phm_v(ii)],[0,round(phm_v(ii)/4,2),round(2*phm_v(ii)/4,2),...
+        round(3*phm_v(ii)/4,2),phm_v(ii)],0,'Particle volume fraction $\overline{\phi}$',consInds,1);
+
+    % save phi figure
+    try
+        print(gcf, ['images/',fld,'image_',fnFull_v{ii},'_phi.png'],'-dpng','-r300');
+    catch
+        mkdir(['images/',fld]);
+        print(gcf, ['images/',fld,'image_',fnFull_v{ii},'_phi.png'],'-dpng','-r300');
+    end
+
 end
 
